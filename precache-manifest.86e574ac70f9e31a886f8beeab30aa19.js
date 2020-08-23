4d2effdc5f7a6f4f7234684f68b8c827b0acce33
Rafael Flores <rafac@unitec.edu>
2020-08-23 17:13:30 -0600

    Updates

---
+++ precache-manifest.86e574ac70f9e31a886f8beeab30aa19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9186385fdbc8186d46b0e72cbd824b7b",
+    "url": "/note-to-pad/index.html"
+  },
+  {
+    "revision": "72a51a1a27ff828a14ad",
+    "url": "/note-to-pad/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ba8b5e6bdb3185c8ae1a",
+    "url": "/note-to-pad/static/css/main.975ce92f.chunk.css"
+  },
+  {
+    "revision": "72a51a1a27ff828a14ad",
+    "url": "/note-to-pad/static/js/2.12ae5d0a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/note-to-pad/static/js/2.12ae5d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8b5e6bdb3185c8ae1a",
+    "url": "/note-to-pad/static/js/main.cb3ec5f4.chunk.js"
+  },
+  {
+    "revision": "a3f224655e29d0d1c622",
+    "url": "/note-to-pad/static/js/runtime-main.06069307.js"
+  }
+]);
